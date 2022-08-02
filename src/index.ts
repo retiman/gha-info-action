4a8c72cdd6adf66848251a33a03eaf046f8173d4
Min Huang <min.huang@alumni.usc.edu>
2022-08-01 21:13:48 -0700

    i need the ts action

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as process from 'process';
+
+const show = (name: string, data: string, outputDirectory?: string) => {
+  core.startGroup(`Show '${name}' context`);
+  core.info(data);
+  if (outputDirectory) {
+    const outputFile = path.join(outputDirectory, `${name}-context.json`);
+    fs.writeFileSync(outputFile, data);
+  }
+  core.endGroup();
+}
+
+const run = async () => {
+  const envContext = core.getInput('env-context', { required: false });
+  const githubContext = core.getInput('github-context', { required: false });
+  const inputsContext = core.getInput('inputs-context', { required: false });
+  const jobContext = core.getInput('job-context', { required: false });
+  const matrixContext = core.getInput('matrix-context', { required: false });
+  const runnerContext = core.getInput('runner-context', { required: false });
+  const secretsContext = core.getInput('secrets-context', { required: false });
+  const stepsContext = core.getInput('steps-context', { required: false });
+  const strategyContext = core.getInput('strategy-context', { required: false });
+  const outputDirectory = core.getInput('output-directory', { required: false });
+
+  core.info('Show environment variables');
+  const data = JSON.stringify(process.env);
+  core.info(data);
+  if (outputDirectory) {
+    const outputFile = path.join(outputDirectory, `env.txt`);
+    fs.writeFileSync(outputFile, data);
+  }
+  core.endGroup();
+
+  show('env', envContext, outputDirectory);
+  show('github', githubContext, outputDirectory);
+  show('inputs', inputsContext, outputDirectory);
+  show('job', jobContext, outputDirectory);
+  show('matrix', matrixContext, outputDirectory);
+  show('runner', runnerContext, outputDirectory);
+  show('secrets', secretsContext, outputDirectory);
+  show('steps', stepsContext, outputDirectory);
+  show('strategy', strategyContext, outputDirectory);
+}
+
+run();
